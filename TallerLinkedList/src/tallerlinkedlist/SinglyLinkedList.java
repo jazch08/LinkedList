b5b162f0750d8314fc2eb32d04c365752a0d1006
Jorge Zambrano <jaaronzambranoch@outlook.es>
2020-11-01 18:13:34 -0500

    Creacion de clase LinkedList y clase Node con sus respectivos atributos

--- TallerLinkedList/src/tallerlinkedlist/SinglyLinkedList.java
+++ TallerLinkedList/src/tallerlinkedlist/SinglyLinkedList.java
@@ -1,5 +1,35 @@
 package tallerlinkedlist;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList <E> {
+    
+    //Clase Node
+    public static class Node <E>{
+        private E element;
+        private Node<E> next;
+        
+        public Node(E e, Node<E> n){
+            element = e;
+            next=n;
+        }
+        
+        public E getElement(){
+            return element;
+        }
+        
+        public Node<E> getNext(){
+            return next;
+        }
+        
+        public void setNext(Node<E> n){
+            next=n;
+        }
+    }
+    //Fin clase Node
+    
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+    public SinglyLinkedList(){};
+    
     
 }
