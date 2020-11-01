340c37c0058858136a138f6d5fec1a9f4cb34b7f
Jorge Zambrano <jaaronzambranoch@outlook.es>
2020-11-01 17:28:15 -0500

    Creacion de proyecto

---
+++ TallerLinkedList/src/tallerlinkedlist/SinglyLinkedList.java
@@ -0,0 +1,5 @@
+package tallerlinkedlist;
+
+public class SinglyLinkedList {
+    
+}
---
+++ TallerLinkedList/src/tallerlinkedlist/TallerLinkedList.java
@@ -0,0 +1,11 @@
+package tallerlinkedlist;
+
+public class TallerLinkedList {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    
+    
+}
