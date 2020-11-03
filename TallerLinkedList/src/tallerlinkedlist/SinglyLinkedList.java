f9f447cb212e90d4e2e4da5ac8984da145711aab
Lizz25 <73801237+Lizz25@users.noreply.github.com>
2020-11-03 14:03:39 -0500

    first() y last()

--- TallerLinkedList/src/tallerlinkedlist/SinglyLinkedList.java
+++ TallerLinkedList/src/tallerlinkedlist/SinglyLinkedList.java
@@ -1,117 +1,144 @@
 package tallerlinkedlist;
 
-public class SinglyLinkedList <E> {
-    
+public class SinglyLinkedList<E> {
+
     //Clase Node
-    public static class Node <E>{
+    public static class Node<E> {
+
         private E element;
         private Node<E> next;
-        
-        public Node(E e, Node<E> n){
+
+        public Node(E e, Node<E> n) {
             element = e;
-            next=n;
+            next = n;
         }
-        
-        public E getElement(){
+
+        public E getElement() {
             return element;
         }
-        
-        public Node<E> getNext(){
+
+        public Node<E> getNext() {
             return next;
         }
-        
-        public void setNext(Node<E> n){
-            next=n;
+
+        public void setNext(Node<E> n) {
+            next = n;
         }
     }
     //Fin clase Node
-    
+
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
-    public SinglyLinkedList(){};
+
+    public SinglyLinkedList() {
+    }
+
+    ;
     
-    public int size(){
+    public int size() {
         return size;
     }
-    
-    public boolean isEmpty(){
-        return size==0;
+
+    public boolean isEmpty() {
+        return size == 0;
     }
-    
-    public E removeFirst(){
-        if(isEmpty()){
+
+    public E first() {
+        if (isEmpty()) {
             return null;
         }
+        return head.getElement();
+    }
+
+    public E last() {
+        if (isEmpty()) {
+            return null;
+        }
+        return tail.getElement();
+    }
+
+    public E removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
         E answer = head.getElement();
         head = head.getNext();
         size--;
-        if(size==0){
+        if (size == 0) {
             tail = null;
         }
         return answer;
-                
+
     }
-    
-    public int find(E element){ //index
-        Node n =head;
-        if (n==null)return -1;
-        int pos=0;
-        while (n.getNext()!=null){
-            if (n.getElement().equals(element))return pos;
-            pos ++;
-            n=n.getNext();
+
+    public int find(E element) { //index
+        Node n = head;
+        if (n == null) {
+            return -1;
+        }
+        int pos = 0;
+        while (n.getNext() != null) {
+            if (n.getElement().equals(element)) {
+                return pos;
+            }
+            pos++;
+            n = n.getNext();
         }
         return -1;
     }
-    
-    public boolean contains (E element){
-        return find(element)!=-1;
+
+    public boolean contains(E element) {
+        return find(element) != -1;
     }
-    
-    public E removeLast(){
-        if(size==0)return null;
-        if (size==1) {
-            E temp= tail.getElement();
-            head= tail=null;
+
+    public E removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        if (size == 1) {
+            E temp = tail.getElement();
+            head = tail = null;
             size--;
             return temp;
         }
-        Node <E> newtail= getPreviusNode(tail);
+        Node<E> newtail = getPreviusNode(tail);
         E temp = tail.getElement();
-        tail=newtail;
-        size --;
+        tail = newtail;
+        size--;
         return temp;
     }
-    
-    private Node<E> getPreviusNode(Node<E> node){
-        if (size<2)return null;
-        Node<E> n0=head;
-        Node <E> n1=head.getNext();
-        while (n1.getNext()!=null){
-            n0=n1;
-            n1=n1.getNext();
-            if (node.equals(n1))return n0;
+
+    private Node<E> getPreviusNode(Node<E> node) {
+        if (size < 2) {
+            return null;
+        }
+        Node<E> n0 = head;
+        Node<E> n1 = head.getNext();
+        while (n1.getNext() != null) {
+            n0 = n1;
+            n1 = n1.getNext();
+            if (node.equals(n1)) {
+                return n0;
+            }
         }
         return null;
     }
-    
-    public E remove(int i){
-        if(isEmpty()){
+
+    public E remove(int i) {
+        if (isEmpty()) {
             return null;
-        }
-        else if(i>size){
+        } else if (i > size) {
             return null;
-        }
-        else if(i == size-1){
+        } else if (i == size - 1) {
             removeLast();
-        }
-        else if(i == 0){
+        } else if (i == 0) {
             removeFirst();
         }
         int pos = 0;
-        for(Node<E> e = tail ; tail.getNext() != null ; e = e.getNext()){
-            if(pos == i-1){
+        for (Node<E> e = tail; tail.getNext() != null; e = e.getNext()) {
+            if (pos == i - 1) {
                 Node<E> temp = e.getNext().getNext();
                 e.setNext(temp);
                 size--;
@@ -121,25 +148,22 @@ public class SinglyLinkedList <E> {
         }
         return null;
     }
-    
-    public boolean add(int index, E element){
-        if(isEmpty()){
+
+    public boolean add(int index, E element) {
+        if (isEmpty()) {
             return false;
-        }
-        else if(index > size){
+        } else if (index > size) {
             return false;
-        }
-        else if(index == size-1){
+        } else if (index == size - 1) {
             //addLast();
             return true;
-        }
-        else if(index == 0){
+        } else if (index == 0) {
             //addFirst();
             return true;
         }
         int pos = 0;
-        for(Node<E> e = tail ; tail.getNext() != null ; e = e.getNext()){
-            if(pos == index-1){
+        for (Node<E> e = tail; tail.getNext() != null; e = e.getNext()) {
+            if (pos == index - 1) {
                 Node<E> temp = e.getNext();
                 Node<E> newNode = new Node(element, temp);
                 e.setNext(newNode);
