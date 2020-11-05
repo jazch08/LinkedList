6b4728c8de3d9743671c5136703eda9879d0172f
Jorge Zambrano <jaaronzambranoch@outlook.es>
2020-11-05 00:27:40 -0500

    Arreglo en los metodos

--- TallerLinkedList/src/tallerlinkedlist/TallerLinkedList.java
+++ TallerLinkedList/src/tallerlinkedlist/TallerLinkedList.java
@@ -3,6 +3,21 @@ package tallerlinkedlist;
 public class TallerLinkedList {
 
     public static void main(String[] args) {
+        SinglyLinkedList l = new SinglyLinkedList();
+        //l[como,h,o]
+        
+        
+        l.addFirst("Como");
+        l.addLast("o");
+        System.out.println(l);
+        l.add(0, "h");
+        System.out.println("Size: "+l.size());
+        
+        System.out.println(l);
+        System.out.println(l.size()+"   "+l.removeLast());
+        System.out.println(l.size()+"   "+l.removeLast());
+        //System.out.println(l.size()+"   "+l.removeLast());
+        System.out.println(l);
         
     }
     
